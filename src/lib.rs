492e20e274bd4228f65bf44ea41c9c10e0cd5d6c
Ronnie Tran <ronnie.tran2@gmail.com>
2020-03-14 22:09:07 -0400

    Update test

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@ mod filesystem;
 // TODO proper tests
 #[cfg(test)]
 mod tests {
-    use crate::filesystem::{FileSystem, Index};
+    use crate::filesystem::{FileSystem, Index, ARCHIVE_INDEX_TYPE};
 
     #[test]
     fn wat() {
         let fs = FileSystem::new("./cache/").unwrap();
 
-        //   fs.read(ARCHIVE_INDEX, 1);
+        fs.get_index(ARCHIVE_INDEX_TYPE, 1).unwrap(); // title entry
 
         /*
 
