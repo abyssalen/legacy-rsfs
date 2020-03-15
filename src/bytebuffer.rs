1d975e05ef01720fc35c3086f2a5fe26ef1f4501
Ronnie Tran <ronnie.tran2@gmail.com>
2020-03-14 22:11:18 -0400

    Add space for better formatting

--- src/bytebuffer.rs
+++ src/bytebuffer.rs
@@ -4,6 +4,7 @@ use std::error::Error;
 pub trait ByteBufferExt {
     fn read_tri_byte(&mut self) -> Result<u32, Box<dyn Error>>;
 }
+
 impl ByteBufferExt for ByteBuffer {
     fn read_tri_byte(&mut self) -> Result<u32, Box<dyn Error>> {
         Ok(u32::from(self.read_u8()?) << 16
