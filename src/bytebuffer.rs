5beadff851d74992041377cfcbb3ecad656f2be7
Ronnie Tran <ronnie.tran2@gmail.com>
2020-03-14 22:08:05 -0400

    Add tri_byte reading support for ByteBuffer

--- src/bytebuffer.rs
+++ src/bytebuffer.rs
@@ -6,6 +6,8 @@ pub trait ByteBufferExt {
 }
 impl ByteBufferExt for ByteBuffer {
     fn read_tri_byte(&mut self) -> Result<u32, Box<dyn Error>> {
-        unimplemented!("reading tri-byte is not implemented!")
+        Ok(u32::from(self.read_u8()?) << 16
+            | u32::from(self.read_u8()?) << 8
+            | u32::from(self.read_u8()?))
     }
 }
